61c01d8d79259ff66e8fcf9eb131af8de699c467
陈健\mrche <mrchenjianz@163.com>
2025-09-12 11:49:02 +0800

    feat: 1、组件二次封装 增加类型提示  2、vue 组件类型声明

--- src/views/home/componnet/MyInput.vue
+++ src/views/home/componnet/MyInput.vue
@@ -2,10 +2,8 @@
 	<div>
 		<el-input v-bind="$attrs">
 			<!-- 具名插槽  用户传什么插槽内容  就给组件透传什么内容 -->
-			 /**
-			  *  template 标签作用是遍历 组件内置插槽 避免一个一个去写
-			  *  同样通过用户传递过来的插槽内容  用template 包裹 保证插槽内容是透传的
-			  */
+			/** * template 标签作用是遍历 组件内置插槽 避免一个一个去写 *
+			同样通过用户传递过来的插槽内容 用template 包裹 保证插槽内容是透传的 */
 			<template v-for="(value, name) in $slots" #[name]="slotData">
 				<!-- 作用插槽 也是透传 -->
 				<slot :name="name" v-bind="slotData"></slot>
@@ -15,6 +13,8 @@
 </template>
 
 <script setup lang="ts">
+import { ElInput, type InputProps } from "element-plus";
+const props = defineProps<Partial<InputProps>>();
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="scss"></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,9 +4,14 @@
 		<p>1、属性透传</p>
 		<p>2、事件透传</p>
 		<p>3、插槽透传</p>
+		<p>4、为组件添加类型</p>
 		<MyInput v-model="input" style="width: 400px" placeholder="Please input">
 			<template #prepend>
-				<el-select v-model="select" placeholder="Select" style="width: 115px">
+				<el-select
+					v-model="selectValue"
+					placeholder="Select"
+					style="width: 115px"
+				>
 					<el-option label="Restaurant" value="1" />
 					<el-option label="Order No." value="2" />
 					<el-option label="Tel" value="3" />
@@ -19,12 +24,16 @@
 	</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import MyInput from "./componnet/MyInput.vue";
-import { Search } from '@element-plus/icons-vue'
+import { Search } from "@element-plus/icons-vue";
+import { defineOptions, ref } from "vue";
 defineOptions({
 	name: "",
 });
+
+let input = ref("");
+let selectValue = ref("");
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped lang="scss"></style>
