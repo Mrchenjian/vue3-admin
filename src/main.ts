a742cc60308edcc8fac571645c72d8ff5019d7ae
陈健\mrche <mrchenjianz@163.com>
2025-09-27 14:45:21 +0800

    feat: 1、 element-plus 全局加载 换成按需加载

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
 import router from './router'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -19,4 +17,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(ElementPlus).use(pinia).use(router).mount('#app')
+app.use(pinia).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import UnoCSS from 'unocss/vite'
+import UnoCSS from "unocss/vite";
 import { fileURLToPath, URL } from "node:url";
 import path from "path";
+
+// 组件按需导入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue(),UnoCSS()],
+	plugins: [
+		vue(),
+		UnoCSS(),
+		AutoImport({
+			// 解析 api
+			resolvers: [ElementPlusResolver()],
+			imports: ["vue", "vue-router","pinia"],
+		}),
+		Components({
+			// 解析组件
+			resolvers: [ElementPlusResolver()],
+			//所有的组件自动加载
+			dirs: ["src/components","src/layout/components"],
+		}),
+	],
 
 	// 配置路径别名  方式一、
 	// resolve: {
